ed61baca75f0c9aae3a118a5fc2b3ca4aa751478
Aniketdhage <aniketdhage90@gmail.com>
2022-01-15 18:53:53 +0530

    added scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Board from './components/Board';
-
+import './root.scss';
 const App = () => (
-  <>
+  <div className="app">
     <h1>TIC TAC TOE</h1>
     <Board />
-  </>
+  </div>
 );
 
 export default App;
