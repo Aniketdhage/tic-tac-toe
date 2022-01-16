9d40326272645a07f4cb3beee777e7c812f1bea6
Aniketdhage <aniketdhage90@gmail.com>
2022-01-16 08:02:36 +0530

    added board functionality and event

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXnext, setisXnext] = useState(false);
+
+  const handleClick = position => {
+    if (board[position]) {
+      return;
+    }
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXnext ? 'x' : '0';
+        }
+        return square;
+      });
+    });
+    setisXnext(prev => !prev);
+  };
+  const renderSquire = position => {
+    return (
+      <Square value={board[position]} onClick={() => handleClick(position)} />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={0} />
-        <Square value={0} />
+        {renderSquire(0)}
+        {renderSquire(1)}
+        {renderSquire(2)}
       </div>
       <div className="board-row">
-        <Square value={0} />
-        <Square value={0} />
-        <Square value={0} />
+        {renderSquire(3)}
+        {renderSquire(4)}
+        {renderSquire(5)}
       </div>
       <div className="board-row">
-        <Square value={0} />
-        <Square value={0} />
-        <Square value={0} />
+        {renderSquire(6)}
+        {renderSquire(7)}
+        {renderSquire(8)}
       </div>
     </div>
   );
